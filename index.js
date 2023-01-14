bcbe769bd6c74c2d6ae108549992b81522d350c1
Ana Carvalho <anapaulacmoura@gmail.com>
2023-01-14 17:59:51 -0300

    updating schemas

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import cors from 'cors';
-import Joi from 'joi';
+import joi from 'joi';
 import dayjs from 'dayjs';
 import express from 'express';
 import dotenv from 'dotenv';
@@ -16,8 +16,21 @@ const mongoClient = new MongoClient(process.env.DATABASE_URL)
 let db
 mongoClient.connect().then( () => {
     db = mongoClient.db()
+});
+
+
+const participant = joi.object({
+    name: joi.string().min(1).required(),
+    lastStatus: joi.number
 })
 
+const message = joi.object({
+    from: joi.string().required(),
+    to: joi.string().min(1).required(),
+    text: joi.string().min(1).required(),
+    type: joi.string().valid("message", "private_message").required(),
+    time: joi.string(),
+})
 
 app.listen(5000, ()=> console.log("The App is running in port 5000"));
 
